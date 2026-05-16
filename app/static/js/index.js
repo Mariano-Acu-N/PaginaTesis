20c90d22d06085072564813d38de09a9f94e03cc
Mariano-Acu-N <94655014+Mariano-Acu-N@users.noreply.github.com>
2026-05-15 21:11:49 -0300

    Update index.js

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -181,7 +181,7 @@ class SimularClimaYBuscarPuntos {
             console.log('Paso');
             Swal.fire({
                 title: '¡Atención!',
-                text: 'Para comenzar la generación de los datos, debe seleccionar una de las opciones configurables.',
+                text: 'Para comenzar la generación de los datos debe seleccionar una de las opciones configurables.',
                 icon: 'warning',
                 confirmButtonText: 'Aceptar',
                 confirmButtonColor: '#3085d6'
