26b4df3fede7fdc31f1c7bd4868a28557ee3d169
Mariano-Acu-N <94655014+Mariano-Acu-N@users.noreply.github.com>
2026-07-01 00:17:10 -0300

    para conectar con el servidor flask
    descomentar api_bp para que conecte con el servidor

--- app/start.py
+++ app/start.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from app.config import Config
 from app.controladores.main import MainController
-# from app.controladores.api import ApiController
+from app.controladores.api import ApiController
 
 class start_app:
     def __init__(self):
         self.config = Config
         self.app = Flask(__name__)
         self.main_bp = MainController().main_bp
-        # self.api_bp = ApiController().api_bp
+        self.api_bp = ApiController().api_bp
         self.configure_app()
         self.register_blueprints()
 
@@ -17,7 +17,7 @@ class start_app:
 
     def register_blueprints(self):
         self.app.register_blueprint(self.main_bp)
-        # self.app.register_blueprint(self.api_bp)
+        self.app.register_blueprint(self.api_bp)
 
     def create_app(self):
         return self.app    
